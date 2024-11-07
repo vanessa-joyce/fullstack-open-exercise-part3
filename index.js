fa513dddf05b9f6deca16f9c1ddd82a7d69aea21
Vanessa Sutter <vanessa@sutter.cloud>
2024-11-07 10:42:49 +0100

    3.9 Phonebook backend step 9

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const cors = require('cors')
 
+const app = express()
 morgan.token('request-body', (request) => {
     return JSON.stringify(request.body)
 })
@@ -14,6 +15,7 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :request-body'))
@@ -100,8 +102,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
